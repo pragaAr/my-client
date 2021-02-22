d9173b65557b64245a454d765eff1a138a5f3f8b
pragaAr <aryapragar@gmail.com>
2021-02-22 09:32:04 +0700

    update

--- application/config/config.php
+++ application/config/config.php
@@ -23,13 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-if (is_cli()) {
-  $config['base_url'] = "";
-} else {
-  $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-  $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-  $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
-}
+$config['base_url'] = 'https://ajm-client.herokuapp.com/';
 
 /*
 |--------------------------------------------------------------------------
