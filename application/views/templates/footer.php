6569de7797f709c46c3432a23dcab3ea878c333f
pragaAr <aryapragar@gmail.com>
2021-02-22 09:28:02 +0700

    update

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -16,18 +16,18 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.nicescroll/3.7.6/jquery.nicescroll.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
-<script src="<?= base_url('assets/assets/js/stisla.js') ?>"></script>
+<script src="https://ajm-client.herokuapp.com/assets/assets/js/stisla.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.10.21/js/dataTables.bootstrap4.min.js"></script>
 
 <!-- Template JS File -->
-<script src="<?= base_url('assets/assets/js/scripts.js') ?>"></script>
-<script src="<?= base_url('assets/assets/js/custom.js') ?>"></script>
+<script src="https://ajm-client.herokuapp.com/assets/assets/js/scripts.js"></script>
+<script src="https://ajm-client.herokuapp.com/assets/assets/js/custom.js"></script>
 
 <!-- Page Specific JS File -->
-<script src="<?= base_url('assets/assets/js/page/index-0.js') ?>"></script>
-<script src="<?= base_url('assets/swal/sweetalert2.all.min.js') ?>"></script>
-<script src="<?= base_url('assets/swal/myscript.js') ?>"></script>
+<script src="https://ajm-client.herokuapp.com/assets/assets/js/page/index-0.js"></script>
+<script src="https://ajm-client.herokuapp.com/assets/swal/sweetalert2.all.min.js"></script>
+<script src="https://ajm-client.herokuapp.com/assets/swal/myscript.js"></script>
 
 <script>
   $(document).ready(function() {
